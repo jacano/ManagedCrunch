3368933651ca7d6c46ef30d29d6a58efd48ebc88
Juan Antonio Cano <jacanosalado@gmail.com>
2017-04-17 19:26:21 +0200

    ManagedCrunch sln added

--- src/build.bat
+++ src/build.bat
@@ -2,4 +2,6 @@ msbuild crunchlib.sln /p:Configuration="Release" /p:Platform="x86"
 msbuild crunchlib.sln /p:Configuration="Release" /p:Platform="x64"
 msbuild crunchlib.sln /p:Configuration="Release" /p:Platform="ARM"
 
+msbuild ManagedCrunch.sln /p:Configuration="Release" /p:Platform="Any CPU"
+
 nuget pack ManagedCrunch.nuspec
