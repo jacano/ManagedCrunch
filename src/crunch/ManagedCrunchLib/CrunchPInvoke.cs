2645d1dba6abb7640d8349840fa72dbbebac0c23
Juan Antonio Cano <jacanosalado@gmail.com>
2017-04-17 18:52:02 +0200

    lib added to repo

---
+++ src/crunch/ManagedCrunchLib/CrunchPInvoke.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WaveCity3D
+{
+    public static class CrunchPInvoke
+    {
+        private const string DllName = "libCrunch.dll";
+        private const CallingConvention DefaultCallingConvention = CallingConvention.Cdecl;
+
+        [DllImport(DllName, CallingConvention = DefaultCallingConvention)]
+        public static unsafe extern uint Crn_get_width(byte* inBuffer, uint inBufferSize);
+
+        [DllImport(DllName, CallingConvention = DefaultCallingConvention)]
+        public static unsafe extern uint Crn_get_height(byte* inBuffer, uint inBufferSize);
+
+        [DllImport(DllName, CallingConvention = DefaultCallingConvention)]
+        public static unsafe extern uint Crn_get_levels(byte* inBuffer, uint inBufferSize);
+
+        [DllImport(DllName, CallingConvention = DefaultCallingConvention)]
+        public static unsafe extern uint Crn_get_dxt_format(byte* inBuffer, uint inBufferSize);
+
+        [DllImport(DllName, CallingConvention = DefaultCallingConvention)]
+        public static unsafe extern uint Crn_get_uncompressed_size(byte* inBuffer, uint inBufferSize);
+
+        [DllImport(DllName, CallingConvention = DefaultCallingConvention)]
+        public static unsafe extern void Crn_decompress(byte* inBuffer, uint inBufferSize, byte* outBuffer, uint outBufferSize);
+    }
+}
