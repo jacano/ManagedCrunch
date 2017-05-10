a31b3dcadfd45e529cc8a50166f2755c57d91faf
Juan Antonio Cano <jacanosalado@gmail.com>
2017-05-10 10:15:27 +0200

    obj intermediate dir updated

--- src/build.bat
+++ src/build.bat
@@ -1,7 +1,10 @@
+set VS2017="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsMSBuildCmd.bat"
+if exist %VS2017% (call %VS2017%)
+
 msbuild crunchlib.sln /p:Configuration="Release" /p:Platform="x86"
 msbuild crunchlib.sln /p:Configuration="Release" /p:Platform="x64"
 msbuild crunchlib.sln /p:Configuration="Release" /p:Platform="ARM"
+msbuild crunchlib.sln /p:Configuration="Release" /p:Platform="ARM64"
 
+nuget restore ManagedCrunch.sln
 msbuild ManagedCrunch.sln /p:Configuration="Release" /p:Platform="Any CPU"
-
-nuget pack ManagedCrunch.nuspec
