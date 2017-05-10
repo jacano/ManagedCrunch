3fc8c23380f71e12fa08b7ed897aa6c57d2303d1
Juan Antonio Cano <jacanosalado@gmail.com>
2017-05-10 11:27:13 +0200

    publish updated

--- src/publish.bat
+++ src/publish.bat
@@ -1,1 +1,6 @@
-nuget pack ManagedCrunch.nuspec
+set PACKAGE_VERSION=1.0.1
+set NUGET_KEY=123
+set PACKAGE_NAME=ManagedCrunch
+
+nuget pack %PACKAGE_NAME%.nuspec -Version %PACKAGE_VERSION%
+nuget push %PACKAGE_NAME%.%PACKAGE_VERSION%.nupkg %NUGET_KEY% -Source https://www.nuget.org/api/v2/package
