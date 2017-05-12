ac59b3204a5b8e6779932207ed7fd158610b0d37
Juan Antonio Cano <jacanosalado@gmail.com>
2017-05-12 10:09:40 +0200

    curl fix

--- src/build.iOS.sh
+++ src/build.iOS.sh
@@ -6,10 +6,11 @@ $NUGET_BIN restore ManagedCrunch.iOS.sln
 $MSBUILD_BIN ManagedCrunch.iOS.sln /p:Configuration="Release" /p:Platform="Any CPU"
 
 ARTIFACT_NAME="ManagedCrunch.dll"
+ARTIFACT_PATH="ManagedCrunch.iOS/bin/Release/$ARTIFACT_NAME"
 
-AUTHORIZATION_TOKEN="Authorization: Bearer "$DROPBOX_KEY
+AUTHORIZATION_TOKEN="Authorization: Bearer $DROPBOX_KEY"
 CONTENT_TYPE="Content-Type: application/octet-stream"
-DROPBOX_ARGS="Dropbox-API-Arg: {\"path\":\"/"$ARTIFACT_NAME"\", \"mode\":\"overwrite\"}"
+DROPBOX_ARGS="Dropbox-API-Arg: {\"path\":\"/$ARTIFACT_NAME\", \"mode\":\"overwrite\"}"
 DROPBOX_URL="https://content.dropboxapi.com/2/files/upload"
 
-curl -X POST $DROPBOX_URL -H $AUTHORIZATION_TOKEN -H $CONTENT_TYPE -H $DROPBOX_ARGS --data-binary @"ManagedCrunch.iOS/bin/Release/"$ARTIFACT_NAME
+curl -X POST --header "$AUTHORIZATION_TOKEN" --header "$CONTENT_TYPE" --header "$DROPBOX_ARGS" --data-binary @"$ARTIFACT_PATH" "$DROPBOX_URL"
