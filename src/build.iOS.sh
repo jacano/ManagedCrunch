9315613822cc66ab273b4ebaa7af9014e908e71d
Juan Antonio Cano <jacanosalado@gmail.com>
2017-05-18 20:22:06 +0200

    build native xcode project

--- src/build.iOS.sh
+++ src/build.iOS.sh
@@ -2,6 +2,11 @@ MONO_DIR="/Library/Frameworks/Mono.framework/Versions/Current/Commands/"
 NUGET_BIN=$MONO_DIR"nuget"
 MSBUILD_BIN=$MONO_DIR"msbuild"
 
+
+pushd crunchxcode
+xcodebuild -configuration Release
+popd
+
 $NUGET_BIN restore ManagedCrunch.iOS.sln
 $MSBUILD_BIN ManagedCrunch.iOS.sln /p:Configuration="Release" /p:Platform="Any CPU"
 
