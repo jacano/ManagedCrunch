ae043608d9572e2793e5ba1e391b4a5bc640719f
jacano <jacanosalado@gmail.com>
2017-08-17 20:19:14 +0200

    progress in secrets folder inside build.bat

--- src/publish.bat
+++ src/publish.bat
@@ -1,4 +1,3 @@
-set NUGET_KEY=123
-
+set /p NUGET_KEY=<secrets\nugetkey.txt
 set /p PACKAGE_VERSION=<version.txt
 nuget push ManagedCrunch.%PACKAGE_VERSION%.nupkg %NUGET_KEY% -Source https://www.nuget.org/api/v2/package
