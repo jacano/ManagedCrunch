0acf888f616cbc9aa819f7c860a8d8234cdb7115
jacano <jacanosalado@gmail.com>
2017-08-23 14:46:05 +0200

    ios target with fix

--- src/grab_ios_build.bat
+++ src/grab_ios_build.bat
@@ -1,12 +1,13 @@
 @echo off
 
-rmdir /s /q lib\ios\
-mkdir lib\ios\
+set ARTIFACT_FOLDER=lib\ios\Release\
+
+rmdir /s /q %ARTIFACT_FOLDER%
+mkdir %ARTIFACT_FOLDER%
 
 set ARTIFACT_NAME=libcrunch.a
-set ARTIFACT_PATH=lib\ios\%ARTIFACT_NAME%
 if not defined DROPBOX_KEY ( set /p DROPBOX_KEY=<secrets\dropboxkey.txt )
 set AUTHORIZATION_TOKEN="Authorization: Bearer %DROPBOX_KEY%"
 set DROPBOX_ARGS="Dropbox-API-Arg: {\"path\":\"/%ARTIFACT_NAME%\"}"
 set DROPBOX_URL="https://content.dropboxapi.com/2/files/download"
-curl -X POST --header %AUTHORIZATION_TOKEN% --header %DROPBOX_ARGS% -o %ARTIFACT_PATH% %DROPBOX_URL%
+curl -X POST --header %AUTHORIZATION_TOKEN% --header %DROPBOX_ARGS% -o %ARTIFACT_FOLDER%%ARTIFACT_NAME% %DROPBOX_URL%
