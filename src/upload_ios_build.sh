9d7cd11679dd8a73d31106f8c338d4e90d674302
jacano <jacanosalado@gmail.com>
2017-08-23 15:29:13 +0200

    publish

--- src/upload_ios_build.sh
+++ src/upload_ios_build.sh
@@ -4,7 +4,7 @@ if [ -z "$DROPBOX_KEY" ]
 fi
 
 ARTIFACT_NAME="libcrunch.a"
-ARTIFACT_PATH="crunch.xcode/build/Release-iphoneos/$ARTIFACT_NAME"
+ARTIFACT_PATH="crunch.iOS/build/$ARTIFACT_NAME"
 AUTHORIZATION_TOKEN="Authorization: Bearer $DROPBOX_KEY"
 CONTENT_TYPE="Content-Type: application/octet-stream"
 DROPBOX_ARGS="Dropbox-API-Arg: {\"path\":\"/$ARTIFACT_NAME\", \"mode\":\"overwrite\"}"
