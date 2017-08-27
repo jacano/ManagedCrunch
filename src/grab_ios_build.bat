fb63a9fcf20613edf25c336f389900bdb2a7ed27
jacano <jacanosalado@gmail.com>
2017-08-27 22:43:16 +0200

    fail to grab or upload binaries

--- src/grab_ios_build.bat
+++ src/grab_ios_build.bat
@@ -10,4 +10,4 @@ if not defined DROPBOX_KEY ( set /p DROPBOX_KEY=<secrets\dropboxkey.txt )
 set AUTHORIZATION_TOKEN="Authorization: Bearer %DROPBOX_KEY%"
 set DROPBOX_ARGS="Dropbox-API-Arg: {\"path\":\"/%ARTIFACT_NAME%\"}"
 set DROPBOX_URL="https://content.dropboxapi.com/2/files/download"
-curl -X POST --header %AUTHORIZATION_TOKEN% --header %DROPBOX_ARGS% -o %ARTIFACT_FOLDER%%ARTIFACT_NAME% %DROPBOX_URL%
+curl -f -X POST --header %AUTHORIZATION_TOKEN% --header %DROPBOX_ARGS% -o %ARTIFACT_FOLDER%%ARTIFACT_NAME% %DROPBOX_URL%
--- src/upload_ios_build.sh
+++ src/upload_ios_build.sh
@@ -10,4 +10,4 @@ CONTENT_TYPE="Content-Type: application/octet-stream"
 DROPBOX_ARGS="Dropbox-API-Arg: {\"path\":\"/$ARTIFACT_NAME\", \"mode\":\"overwrite\"}"
 DROPBOX_URL="https://content.dropboxapi.com/2/files/upload"
 
-curl -X POST --header "$AUTHORIZATION_TOKEN" --header "$CONTENT_TYPE" --header "$DROPBOX_ARGS" --data-binary @"$ARTIFACT_PATH" "$DROPBOX_URL"
+curl -f -X POST --header "$AUTHORIZATION_TOKEN" --header "$CONTENT_TYPE" --header "$DROPBOX_ARGS" --data-binary @"$ARTIFACT_PATH" "$DROPBOX_URL"
