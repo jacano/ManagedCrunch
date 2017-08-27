937bce9257f88e609ebde6e19578e6cd470f749a
jacano <jacanosalado@gmail.com>
2017-08-28 00:27:02 +0200

    progress on flat nuget

--- build.bat
+++ build.bat
@@ -14,3 +14,23 @@ msbuild ManagedCrunch\ManagedCrunch.csproj /p:Configuration="Release" /p:Platfor
 msbuild ManagedCrunch.iOS\ManagedCrunch.iOS.csproj /p:Configuration="Release" /p:Platform="AnyCPU" /p:OutputPath="..\..\output\ManagedCrunch.iOS"
 
 popd
+
+xcopy /F /R /Y /I output\android\x86\Release\libCrunch.so nuget\build\MonoAndroid\android\x86\*
+xcopy /F /R /Y /I output\android\x64\Release\libCrunch.so nuget\build\MonoAndroid\android\x86_64\*
+xcopy /F /R /Y /I output\android\ARM\Release\libCrunch.so nuget\build\MonoAndroid\android\armeabi\*
+xcopy /F /R /Y /I output\android\ARM\Release\libCrunch.so nuget\build\MonoAndroid\android\armeabi-v7a\*
+xcopy /F /R /Y /I output\android\ARM64\Release\libCrunch.so nuget\build\MonoAndroid\android\arm64-v8a\*
+
+xcopy /F /R /Y /I output\uwp\Win32\Release\libCrunch.dll nuget\build\uap10.0\uwp\x86\*
+xcopy /F /R /Y /I output\uwp\x64\Release\libCrunch.dll nuget\build\uap10.0\uwp\x64\*
+xcopy /F /R /Y /I output\uwp\ARM\Release\libCrunch.dll nuget\build\uap10.0\uwp\arm\*
+
+xcopy /F /R /Y /I output\windows\Win32\Release\libCrunch.dll nuget\build\net45\windows\x86\*
+xcopy /F /R /Y /I output\windows\x64\Release\libCrunch.dll nuget\build\net45\windows\x64\*
+
+call grab_ios_build.bat
+xcopy /F /R /Y /I output\ios\Release\libcrunch.a nuget\build\XamariniOS\ios\*
+
+xcopy /F /R /Y /I output\ManagedCrunch\ManagedCrunch.dll nuget\lib\netstandard1.1\*
+xcopy /F /R /Y /I output\ManagedCrunch.iOS\ManagedCrunch.dll nuget\lib\XamariniOS\*
+
