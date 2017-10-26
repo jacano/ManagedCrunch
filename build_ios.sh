d5bcb64f1edd193a60fce7c49ccbabce47379a42
jacano <jacanosalado@gmail.com>
2017-10-26 14:00:44 +0200

    helper removed

--- build_ios.sh
+++ build_ios.sh
@@ -1,5 +1,27 @@
-my_dir="$(dirname "$0")"
-"$my_dir/helper.sh" 
+ZipAndUploadToDropbox () {
+
+	REMOTE_FOLDER=$1
+	ARTIFACT_NAME=$2
+	BUILD_DIR=$3
+	
+    if [ -z "$DROPBOX_KEY" ] 
+		then
+		DROPBOX_KEY=$(<secrets/dropboxkey.txt)
+	fi
+
+	DROPBOX_ARGS="Dropbox-API-Arg: {\"path\":\"$REMOTE_FOLDER/$ARTIFACT_NAME\", \"mode\":\"overwrite\"}"
+	DROPBOX_URL="https://content.dropboxapi.com/2/files/upload"
+	AUTHORIZATION_TOKEN="Authorization: Bearer $DROPBOX_KEY"
+	CONTENT_TYPE="Content-Type: application/octet-stream"
+
+	pushd $BUILD_DIR
+
+	zip -r $ARTIFACT_NAME .
+
+	curl -f -X POST --header "$AUTHORIZATION_TOKEN" --header "$CONTENT_TYPE" --header "$DROPBOX_ARGS" --data-binary @"$ARTIFACT_NAME" "$DROPBOX_URL"
+
+	popd
+}
 
 pushd src/crunch.iOS
 
