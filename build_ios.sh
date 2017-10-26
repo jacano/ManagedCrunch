d9090f9fe38ad7991500a1461ac7e3bd011bad86
jacano <jacanosalado@gmail.com>
2017-10-26 15:26:53 +0200

    function removed

--- build_ios.sh
+++ build_ios.sh
@@ -1,28 +1,3 @@
-ZipAndUploadToDropbox () {
-
-	REMOTE_FOLDER=$1
-	ARTIFACT_NAME=$2
-	BUILD_DIR=$3
-	
-    if [ -z "$DROPBOX_KEY" ] 
-		then
-		DROPBOX_KEY=$(<secrets/dropboxkey.txt)
-	fi
-
-	DROPBOX_ARGS="Dropbox-API-Arg: {\"path\":\"$REMOTE_FOLDER/$ARTIFACT_NAME\", \"mode\":\"overwrite\"}"
-	DROPBOX_URL="https://content.dropboxapi.com/2/files/upload"
-	AUTHORIZATION_TOKEN="Authorization: Bearer $DROPBOX_KEY"
-	CONTENT_TYPE="Content-Type: application/octet-stream"
-
-	pushd $BUILD_DIR
-
-	zip -r $ARTIFACT_NAME .
-
-	curl -f -X POST --header "$AUTHORIZATION_TOKEN" --header "$CONTENT_TYPE" --header "$DROPBOX_ARGS" --data-binary @"$ARTIFACT_NAME" "$DROPBOX_URL"
-
-	popd
-}
-
 pushd src/crunch.iOS
 
 xcodebuild -configuration Release -sdk iphonesimulator clean build
@@ -38,4 +13,25 @@ popd
 
 popd
 
-ZipAndUploadToDropbox "crunch" "ios.zip" "src/crunch.ios/build"
+
+REMOTE_FOLDER="crunch"
+ARTIFACT_NAME="ios.zip"
+BUILD_DIR="src/crunch.ios/build"
+
+if [ -z "$DROPBOX_KEY" ]
+	then
+	DROPBOX_KEY=$(<secrets/dropboxkey.txt)
+fi
+
+DROPBOX_ARGS="Dropbox-API-Arg: {\"path\":\"$REMOTE_FOLDER/$ARTIFACT_NAME\", \"mode\":\"overwrite\"}"
+DROPBOX_URL="https://content.dropboxapi.com/2/files/upload"
+AUTHORIZATION_TOKEN="Authorization: Bearer $DROPBOX_KEY"
+CONTENT_TYPE="Content-Type: application/octet-stream"
+
+pushd $BUILD_DIR
+
+zip -r $ARTIFACT_NAME .
+
+curl -f -X POST --header "$AUTHORIZATION_TOKEN" --header "$CONTENT_TYPE" --header "$DROPBOX_ARGS" --data-binary @"$ARTIFACT_NAME" "$DROPBOX_URL"
+
+popd
