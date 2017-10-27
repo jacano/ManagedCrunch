0d370947e967d09f9bcf3d9957281c6365b86b73
jacano <jacanosalado@gmail.com>
2017-10-27 13:20:43 +0200

    artifacts in repo :S

--- build.ps1
+++ build.ps1
@@ -8,3 +8,8 @@ nuget.exe restore ManagedCrunch.sln
 msbuild ManagedCrunch/ManagedCrunch.csproj /p:Configuration="Release" /p:Platform="AnyCPU"
 
 popd
+
+Copy-Item artifacts/windows/* src/ManagedCrunch/windows -Force -Recurse
+Copy-Item artifacts/uwp/* src/ManagedCrunch/uwp -Force -Recurse
+Copy-Item artifacts/android/* src/ManagedCrunch/android -Force -Recurse
+#Copy-Item artifacts/ios/* src/ManagedCrunch/ios -Force -Recurse
--- build_android.ps1
+++ build_android.ps1
@@ -4,9 +4,7 @@ pushd src/crunch.Android
 
 ndk-build NDK_PROJECT_PATH=. NDK_APPLICATION_MK=./Application.mk
 
-Rename-Item libs build
-
 popd
 
 New-Item "artifacts" -type directory -force
-Move-Item "src/crunch.android/build" "artifacts/android" -Force
+Move-Item "src/crunch.android/libs" "artifacts/android" -Force
