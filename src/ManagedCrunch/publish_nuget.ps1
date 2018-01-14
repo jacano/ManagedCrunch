601714f19884db60b475545a7487f1385f7d7add
Juan Antonio Cano <jacanosalado@gmail.com>
2018-01-14 21:48:45 +0100

    moving

--- src/ManagedCrunch/publish_nuget.ps1
+++
@@ -1,4 +0,0 @@
-if not defined NUGET_KEY ( set /p NUGET_KEY=<..\secrets\nugetkey.txt )
-set /p PACKAGE_VERSION=<version.txt
-
-nuget push ManagedCrunch.%PACKAGE_VERSION%.nupkg %NUGET_KEY% -Source https://www.nuget.org/api/v2/package
