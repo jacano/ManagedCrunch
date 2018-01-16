3b84da89b8fea193240cf63fe89b4731e9a8fa0c
Juan Antonio Cano <jacanosalado@gmail.com>
2018-01-16 18:37:25 +0100

    progress

--- create_nuget.ps1
+++ create_nuget.ps1
@@ -1,8 +1,5 @@
-#set /p PACKAGE_VERSION=<version.txt
+if (-not (Test-Path env:PACKAGE_VERSION)) { $env:PACKAGE_VERSION = Get-Content .\nuget_version.txt -Raw }
+if (-not (Test-Path env:NUGET_KEY)) { $env:NUGET_KEY = Get-Content .\secrets\nuget_key.txt -Raw }
 
-#nuget pack ManagedCrunch.nuspec -Version %PACKAGE_VERSION%
-
-if not defined NUGET_KEY ( set /p NUGET_KEY=<..\secrets\nugetkey.txt )
-set /p PACKAGE_VERSION=<version.txt
-
-nuget push ManagedCrunch.%PACKAGE_VERSION%.nupkg %NUGET_KEY% -Source https://www.nuget.org/api/v2/package
+nuget pack ManagedCrunch.nuspec -Version $env:PACKAGE_VERSION
+nuget push ManagedCrunch.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://www.nuget.org/api/v2/package
