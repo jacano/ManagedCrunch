c2f665b9ca6c4ea042b15ef8a735391040dc20eb
Juan Antonio Cano <jacanosalado@gmail.com>
2018-01-16 19:40:35 +0100

    progress

---
+++ publish_nuget.ps1
@@ -0,0 +1,4 @@
+$env:PACKAGE_VERSION = Get-Content .\nuget_version.txt -Raw
+if (-not (Test-Path env:NUGET_KEY)) { $env:NUGET_KEY = Get-Content .\secrets\nuget_key.txt -Raw }
+
+nuget.exe push ManagedCrunch.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://www.nuget.org/api/v2/package
