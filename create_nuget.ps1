666a12197281ca79061ac3cbec53c69ce360df02
Juan Antonio Cano <jacanosalado@gmail.com>
2018-01-16 18:44:03 +0100

    progress

--- create_nuget.ps1
+++ create_nuget.ps1
@@ -1,5 +1,5 @@
 if (-not (Test-Path env:PACKAGE_VERSION)) { $env:PACKAGE_VERSION = Get-Content .\nuget_version.txt -Raw }
 if (-not (Test-Path env:NUGET_KEY)) { $env:NUGET_KEY = Get-Content .\secrets\nuget_key.txt -Raw }
 
-nuget pack ManagedCrunch.nuspec -Version $env:PACKAGE_VERSION
-nuget push ManagedCrunch.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://www.nuget.org/api/v2/package
+nuget.exe pack src/ManagedCrunch/ManagedCrunch.nuspec -Version $env:PACKAGE_VERSION
+#nuget.exe push ManagedCrunch.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://www.nuget.org/api/v2/package
