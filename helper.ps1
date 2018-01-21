a9b5df6950ee90ecacfdc6aede201171c3177b19
jacano <jacanosalado@gmail.com>
2018-01-21 22:26:21 +0100

    progress

--- helper.ps1
+++ helper.ps1
@@ -2,12 +2,18 @@ function PrepareVSEnviroment()
 {
 	# https://stackoverflow.com/a/2124759/104185
 
-	pushd "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\Enterprise\Common7\Tools"
-	cmd /c "VsDevCmd.bat&set" |
-	foreach {
-	  if ($_ -match "=") {
-		$v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
-	  }
+	$vs2017Folder = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\"
+	$vs2017EnvBat = "VsDevCmd.bat"
+	
+	if (Test-Path  ($vs2017Folder + $vs2017EnvBat))
+	{
+		pushd $vs2017Folder
+		cmd /c $vs2017EnvBat + "&set" |
+		foreach {
+		  if ($_ -match "=") {
+			$v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+		  }
+		}
+		popd
 	}
-	popd
 }
