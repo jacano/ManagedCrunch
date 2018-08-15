09bbb43380d348f4d856165db8c1b77c568b0f88
Juan Antonio Cano <jacanosalado@gmail.com>
2018-08-16 01:47:36 +0200

    adding bindings and native

--- create_nuget.ps1
+++ create_nuget.ps1
@@ -1,3 +1,4 @@
 $env:PACKAGE_VERSION = Get-Content .\nuget_version.txt -Raw
 
-nuget.exe pack src/ManagedCrunch/ManagedCrunch.nuspec -Version $env:PACKAGE_VERSION
+nuget.exe pack src/ManagedCrunch/ManagedCrunch.Bindings/ManagedCrunch.Bindings.nuspec -Version $env:PACKAGE_VERSION
+nuget.exe pack src/ManagedCrunch/ManagedCrunch.Native/ManagedCrunch.Native.nuspec -Version $env:PACKAGE_VERSION
--- move_artifacts.ps1
+++ move_artifacts.ps1
@@ -1,4 +1,4 @@
-Copy-Item artifacts/windows/* src/ManagedCrunch/build/net45 -Force -Recurse
-Copy-Item artifacts/uwp/* src/ManagedCrunch/build/uap10.0 -Force -Recurse
-Copy-Item artifacts/android/* src/ManagedCrunch/build/MonoAndroid -Force -Recurse
-Copy-Item artifacts/ios/* src/ManagedCrunch/build/XamariniOS -Force -Recurse
+Copy-Item artifacts/windows/* src/ManagedCrunch/ManagedCrunch.Native/build/net45 -Force -Recurse
+Copy-Item artifacts/uwp/* src/ManagedCrunch/ManagedCrunch.Native/build/uap10.0 -Force -Recurse
+Copy-Item artifacts/android/* src/ManagedCrunch/ManagedCrunch.Native/build/MonoAndroid -Force -Recurse
+Copy-Item artifacts/ios/* src/ManagedCrunch/ManagedCrunch.Native/build/XamariniOS -Force -Recurse
