fd2331e0750d7520e2366313eebabf80ac8e0f35
Juan Antonio Cano <jacanosalado@gmail.com>
2018-09-07 23:53:53 +0200

    Base package added

--- create_nuget.ps1
+++ create_nuget.ps1
@@ -2,3 +2,4 @@ $env:PACKAGE_VERSION = Get-Content .\nuget_version.txt -Raw
 
 nuget.exe pack src/ManagedCrunch/ManagedCrunch.Bindings/ManagedCrunch.Bindings.nuspec -Version $env:PACKAGE_VERSION -OutputDirectory nuget
 nuget.exe pack src/ManagedCrunch/ManagedCrunch.Native/ManagedCrunch.Native.nuspec -Version $env:PACKAGE_VERSION -OutputDirectory nuget
+nuget.exe pack src/ManagedCrunch/ManagedCrunch/ManagedCrunch.nuspec -Version $env:PACKAGE_VERSION -OutputDirectory nuget
