d8cd79b1de7cb5601140b599968ea463bcecd9ef
Juan Antonio Cano <jacanosalado@gmail.com>
2018-09-07 18:08:24 +0200

    -publish fix

--- publish_nuget.ps1
+++ publish_nuget.ps1
@@ -1,5 +1,5 @@
 $env:PACKAGE_VERSION = Get-Content .\nuget_version.txt -Raw
 if (-not (Test-Path env:NUGET_KEY)) { $env:NUGET_KEY = Get-Content .\secrets\nuget_key.txt -Raw }
 
-nuget.exe push ManagedCrunch.Bindings.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://www.nuget.org/api/v2/package
-nuget.exe push ManagedCrunch.Native.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://www.nuget.org/api/v2/package
+nuget.exe push nuget/ManagedCrunch.Bindings.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://www.nuget.org/api/v2/package
+nuget.exe push nuget/ManagedCrunch.Native.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://www.nuget.org/api/v2/package
