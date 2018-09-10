d3e47d1b6aaacd4ae8e0ff5c45dfe0dc55f457c5
Juan Antonio Cano <jacanosalado@gmail.com>
2018-09-10 09:13:49 +0200

    Progress

--- src/crunch.Android/Android.mk
+++ src/crunch.Android/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libcrunch
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../crunch/
 LOCAL_SRC_FILES := $(LOCAL_PATH)/../crunch/crn.cpp
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../crunch/
 include $(BUILD_SHARED_LIBRARY)
 
