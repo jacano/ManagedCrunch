9029f37c068d5572864c448aa5a21a2eab502f65
Juan Antonio Cano <jacanosalado@gmail.com>
2018-10-27 14:28:46 +0200

    progress

--- publish_nuget.ps1
+++ publish_nuget.ps1
@@ -1,6 +1,6 @@
 $env:PACKAGE_VERSION = Get-Content .\nuget_version.txt -Raw
 if (-not (Test-Path env:NUGET_KEY)) { $env:NUGET_KEY = Get-Content .\secrets\nuget_key.txt -Raw }
 
-nuget.exe push nuget/ManagedCrunch.Bindings.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://www.nuget.org/api/v2/package
-nuget.exe push nuget/ManagedCrunch.Native.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://www.nuget.org/api/v2/
-nuget.exe push nuget/ManagedCrunch.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://www.nuget.org/api/v2/package
+nuget.exe push nuget/ManagedCrunch.Bindings.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://api.nuget.org/v3/index.json
+nuget.exe push nuget/ManagedCrunch.Native.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://api.nuget.org/v3/index.json
+nuget.exe push nuget/ManagedCrunch.$env:PACKAGE_VERSION.nupkg $env:NUGET_KEY -Source https://api.nuget.org/v3/index.json
